b7a4aa210cbd08b2c5eb8c79828547457ace31f7
jacobdadoun <jacobdadoun@gmail.com>
2020-11-17 22:38:12 -0500

    Working Prime finder

--- HelloGithubClassroom/Program.cs
+++ HelloGithubClassroom/Program.cs
@@ -1,12 +1,95 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
 
 namespace HelloGithubClassroom
 {
-    class Program
+    
+    public class PrimeEnumerator : IEnumerator<long>
     {
-        static void Main(string[] args)
+
+        public long Current { get; private set; } = 2;
+        
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
         {
-            Console.WriteLine("Hello World!");
+
+
+            if (Current == 100) // <-- Set limit here
+            {
+                Console.WriteLine("--reached long overflow--");
+                return false;
+            }
+            Current++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            Current = 0;
+        }
+
+        public void Dispose()
+        {
         }
     }
+    
+    // --------------------------------------------------------
+    // ---------------------------------------------------------
+    
+    public class PrimeEnumerable : IEnumerable<long>
+    {
+
+        IEnumerator<long> IEnumerable<long>.GetEnumerator()
+        {
+            return new PrimeEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new PrimeEnumerator();
+        }
+        
+        
+
+        public static bool IsPrime(object numberPassed)
+        {
+            var number = 0L;
+            number = (long) numberPassed;
+            
+            if (number < 2) return false;
+            if (number % 2 == 0) return (number == 2);
+            int root = (int)Math.Sqrt((double)number);
+            for (int i = 3; i <= root; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            var primeEnumerable = new PrimeEnumerable();
+
+            var enumerator = primeEnumerable.GetEnumerator();
+            
+            while (enumerator.MoveNext())
+            {
+                if (IsPrime(enumerator.Current))
+                {
+                    Console.WriteLine($"Prime number: {enumerator.Current}");
+                }
+                
+                
+            }
+            
+            
+            
+        }
+    }
+
 }
