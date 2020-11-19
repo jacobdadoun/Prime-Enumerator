9e6d18634779d62e831eac5a3bb08d4578cc0aed
jacobdadoun <jacobdadoun@gmail.com>
2020-11-18 23:21:50 -0500

    Now pushing to two repos! cool! Appears to have issues importing a project. How to set Main 2 times?

---
+++ ClassLibrary1/Fibonacci.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Fibonacci
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ FibonacciClassFile/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FibonacciClassFile
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
